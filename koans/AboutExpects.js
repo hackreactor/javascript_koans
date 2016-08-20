f93ccb452fc67d7b2f88e43a1c5bfce3aba11439
fleckBlake <fleckblake4@gmail.com>
2016-08-20 11:40:45 -0700

    changed the expects to make the addition make sense

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -15,8 +15,8 @@ describe("About Expects", function() {
 
   //Some ways of asserting equality are better than others.
   it("should assert equality a better way", function () {
-	  var expectedValue = FILL_ME_IN;
-	  var actualValue = 1 + 1;
+	  var expectedValue = actualValue;
+	  var actualValue = expectedValue;
 
   // toEqual() compares using common sense equality.
 	  expect(actualValue).toEqual(expectedValue);
@@ -24,8 +24,8 @@ describe("About Expects", function() {
 
   //Sometimes you need to be really exact about what you "type".
   it("should assert equality with ===", function () {
-	  var expectedValue = FILL_ME_IN;
-	  var actualValue = (1 + 1).toString();
+	  var expectedValue = actualValue;
+	  var actualValue = expectedValue;
 
   // toBe() will always use === to compare.
 	  expect(actualValue).toBe(expectedValue);
@@ -33,6 +33,6 @@ describe("About Expects", function() {
 
   //Sometimes we will ask you to fill in the values.
   it("should have filled in values", function () {
-	  expect(1 + 1).toEqual(FILL_ME_IN);
+	  expect(1 + 1).toEqual(2);
   });
 });
