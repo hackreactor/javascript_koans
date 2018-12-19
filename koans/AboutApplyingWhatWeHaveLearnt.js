80c91bdbc8f7cb1fa40bab18e248002e56ea593b
Delos Elder <dmelder@gmail.com>
2018-12-19 13:44:56 -0500

    Completed javscript koans course

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,15 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var noNuts = products.filter(function (x) {
+        return x.containsNuts === false;
+      });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      var productsICanEat = noNuts.filter(function (x) {
+        return x.ingredients.all(function(y) { return y !== 'mushrooms'}
+      });
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
